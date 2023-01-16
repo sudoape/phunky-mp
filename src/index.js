c016b5e911c4e23b04340f30548369cc190e2c5c
sudoape <tokosapos@protonmail.com>
2023-01-16 02:05:08 +0200

    sentry monitoring

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import './index.css'
 import Web3 from 'web3'
 import detectEthereumProvider from '@metamask/detect-provider'
 import { web3ProviderURL } from 'consts'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 
 const web3 = new Web3(web3ProviderURL)
+
+Sentry.init({
+  dsn: 'https://e79c03ee5546443993243b72dd081237@o4504510885527552.ingest.sentry.io/4504510899683328',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.5,
+})
 
 const Application = () => {
   useEffect(() => {
